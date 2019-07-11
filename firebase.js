d5b69936fd35add21510b96862329b13d9995969
ALESSANDRO PINHO LENOVO <eletricapinho@gmail.com>
2019-07-11 01:30:39 -0300

    alteraçoes com duas lampadas funcionado

--- firebase.js
+++ firebase.js
@@ -1,28 +1,37 @@
 var res = document.getElementById('resultado')
-let lamp = document.querySelector('#lamp');
-let lamp1 = document.querySelector('#lamp1');
-
-
+var res1 = document.getElementById('resultado1')
+var res2 = document.getElementById('resultado2')
+var res3 = document.getElementById('resultado3')
 
 
 let database = firebase.database();  // instacia para ler ou escrever dados no firebase
 
+function status(){  // chamada no body  onlond chama as demais funçoes 
 
-function desligar(){
+status_escritorio()  
+status_varanda()
+   
+
+}
+
+
+
+function desligar_escritorio(){
     database.ref('Controle/').set(1);  /// muda o banco e desliga o rele 
  //  status()
  
 }
 
 
-function ligar(){
+function ligar_escritorio(){
     database.ref('Controle/').set(0);   // muda o banco e Liga  o rele
   //  status()
 
 }
 
 
-function status(){    ///função responsavel por atualizar os estados do rele 
+
+function status_escritorio(){    ///função responsavel por atualizar os estados do rele 
     
 res.innerHTML = `INICIANDO...`   ///teste se função foi acionada 
 res.style.background = 'green'
@@ -43,4 +52,47 @@ database.ref('Controle/').on('value', snapshot => {    ///essa para é mais impo
          res.innerText = "[ERRO]"
             break;
     }});
+
+}
+
+
+
+
+
+function desligar_varanda(){
+    database.ref('luzVaranda/').set(1);  /// muda o banco e desliga o rele 
+ //  status()
+ 
+}
+
+
+function ligar_varanda(){
+    database.ref('luzVaranda/').set(0);   // muda o banco e Liga  o rele
+  //  status()
+
+}
+
+
+
+function status_varanda(){    ///função responsavel por atualizar os estados do rele 
+    
+    res1.innerHTML = `INICIANDO...`   ///teste se função foi acionada 
+    res1.style.background = 'green'
+    
+    database.ref('luzVaranda/').on('value', snapshot => {    ///essa para é mais importante pois é aqui que a magica acontece 
+       var  buttonState = snapshot.val();  //// snapshot é referencia 
+    
+       switch(buttonState){
+           case 0:
+            res1.innerText = "LIGADO"
+            res1.style.background = 'red'
+                break;
+            case 1:
+            res1.innerText = "DESLIGADO"
+            res1.style.background = 'black'
+                break;
+            default:
+             res1.innerText = "[ERRO]"
+                break;
+        }});
 }
